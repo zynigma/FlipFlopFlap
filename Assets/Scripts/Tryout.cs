5065939456143c3f33344cc6abb578248c0285c7
Zynigma <zynigma25@gmail.com>
2015-07-07 10:14:15 -0500

    initial commit, hope this works...

---
+++ Assets/Scripts/Tryout.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tryout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("This does work, can't wait to get it ALL working right!");
+	}
+	
+	// Update is called once per frames
+	void Update () {
+	
+	}
+}
